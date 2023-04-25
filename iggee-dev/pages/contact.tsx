9db8f93ab7788e8ddbad618af84cb9022f47153d
matteo <hu.igor@yahoo.com>
2023-04-25 10:24:26 -0400

    Vercel Yells At Me For not using className

--- iggee-dev/pages/contact.tsx
+++ iggee-dev/pages/contact.tsx
@@ -18,13 +18,13 @@ export default function Home() {
           <h2 className="p-5 font-blog text-2xl text-center">Talk to me: </h2>
           <div className="text-xl text-center">
             <a href="tianci@iggee.dev">tianci@iggee.dev</a>
-            <a href="https://github.com/iggeehu" class="icon github" title="GitHub">
+            {/* <a href="https://github.com/iggeehu" className="icon github" title="GitHub">
               
             </a>
             
-            <a href="https://www.linkedin.com/in/iggee" class="icon linkedin" title="LinkedIn">
-               
-            </a>
+            <a href="https://www.linkedin.com/in/iggee" className="icon linkedin" title="LinkedIn"> */}
+{/*                
+            </a> */}
            
             
           </div>
